ecfc0b9f700c6f2aecba6457672d62596efa9c91
po <lilymiyumartin@gmail.com>
2025-07-20 23:56:30 +0800

    ALU

--- rtl/ALUctrl_MUX.sv
+++ rtl/ALUctrl_MUX.sv
@@ -1,3 +1,4 @@
+//EQ == equal to or greater than
 module ALUctrl_MUX #(
     DATA_WIDTH = 32
 ) (
@@ -7,6 +8,34 @@ module ALUctrl_MUX #(
     output  logic [DATA_WIDTH-1:0]  ALUout,
     output  logic EQ
 );
-    assign out = ALUsrc ? in1 : in0;
+    always_comb begin
+        case (ALUctrl)
+            3'b0: begin
+                ALUout = in0 + in1;
+                EQ = (in0 == in1);
+            end
+            3'b001: begin
+                ALUout = in0 - in1; 
+                EQ = (in0 == in1);
+            end
+            3'b010: begin
+                ALUout = in0 & in1; 
+                EQ = (in0 == in1);
+            end
+            3'b011: begin
+                ALUout = in0 | in1;
+                EQ = (in0 == in1);
+            end
+            3'b100: begin
+                ALUout = in0 ^ in1;
+                EQ = (in0 == in1);
+            end
+            default: begin
+                ALUout = '0; 
+                EQ = 1'b0;
+            end
+        endcase
+    end
+
 
 endmodule
--- rtl/extend.sv
+++ rtl/extend.sv
@@ -1,7 +1,7 @@
 module extend(
     input logic [12:0] instr,
     output logic [31:0] ext_instr
-)
+);
 
     assign ext_instr = 32'(signed'(instr));
 
