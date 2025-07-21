18e452700fd5bf69e9c00cd7fce97c3414edd043
po <lilymiyumartin@gmail.com>
2025-07-21 15:11:29 +0800

    extend fixed for both

--- rtl/extend.sv
+++ rtl/extend.sv
@@ -1,10 +1,17 @@
-//
 module extend(
-    input   logic [12:0] instr,
+    input   logic [31:0] instr,
     input   logic ImmSrc,
     output  logic [31:0] ext_instr
 );
 
-    assign ext_instr = 32'(signed'(instr));
+    always_comb begin
+        if (ImmSrc) begin
+            //I type
+            ext_instr = {{20{instr[31]}}, instr[31:20]}; // I-type and S-type
+        end else begin
+            // S type
+            ext_instr = {20'b0, instr[31:20]}; // U-type and J-type
+        end
+    end
 
 endmodule
