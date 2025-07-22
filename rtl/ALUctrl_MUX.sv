a74b52f151609f9b39e00e0dbe500a01975aefe2
thebeans342 <lm2923@ic.ac.uk>
2025-07-22 12:04:52 +0800

    combined muxes

--- rtl/ALUctrl_MUX.sv
+++ rtl/ALUctrl_MUX.sv
@@ -2,33 +2,39 @@
 module ALUctrl_MUX #(
     DATA_WIDTH = 32
 ) (
-    input   logic [DATA_WIDTH-1:0]  in0,
-    input   logic [DATA_WIDTH-1:0]  in1,
+    input   logic [DATA_WIDTH-1:0]  ALUop1,
+    input   logic [DATA_WIDTH-1:0]  regop2,
     input   logic [2:0]             ALUctrl,
+    input   logic                   ALUsrc,
+    input   logic [DATA_WIDTH-1:0]  ImmOp,
     output  logic [DATA_WIDTH-1:0]  ALUout,
     output  logic EQ
 );
+
+    logic [DATA_WIDTH-1:0] ALUop2;
+    assign ALUop2  = ALUsrc ? ImmOp : regop2;
+
     always_comb begin
         case (ALUctrl)
             3'b0: begin
-                ALUout = in0 + in1;
-                EQ = (in0 == in1);
+                ALUout = ALUop1 + ALUop2;
+                EQ = (ALUop1 == ALUop2);
             end
             3'b001: begin
-                ALUout = in0 - in1; 
-                EQ = (in0 == in1);
+                ALUout = ALUop1 - ALUop2; 
+                EQ = (ALUop1 == ALUop2);
             end
             3'b010: begin
-                ALUout = in0 & in1; 
-                EQ = (in0 == in1);
+                ALUout = ALUop1 & ALUop2; 
+                EQ = (ALUop1 == ALUop2);
             end
             3'b011: begin
-                ALUout = in0 | in1;
-                EQ = (in0 == in1);
+                ALUout = ALUop1 | ALUop2;
+                EQ = (ALUop1 == ALUop2);
             end
             3'b100: begin
-                ALUout = in0 ^ in1;
-                EQ = (in0 == in1);
+                ALUout = ALUop1 ^ ALUop2;
+                EQ = (ALUop1 == ALUop2);
             end
             default: begin
                 ALUout = '0; 
--- rtl/top.sv
+++ rtl/top.sv
@@ -7,7 +7,25 @@ module top #(
 );
     assign a0 = 32'd5;
     logic [DATA_WIDTH-1:0] instr;
+    logic EQ;
+    logic PCsrc;
+    logic ResultSrc;
+    logic MemWrite;
+    logic [2:0] ALUctrl;
+    logic ALUsrc;
+    logic [1:0] ImmSrc;
+    logic RegWrite;
+
+    logic we;
+    logic [DATA_WIDTH-1:0] ALUout; //wd
+    logic [DATA_WIDTH-1:0] ALUop1;
+    logic [DATA_WIDTH-1:0] regOp2;
+    logic [DATA_WIDTH-1:0] ALUop2;
+    
     logic [DATA_WIDTH-1:0] ImmOp;
+
     logic [DATA_WIDTH-1:0] PC_out;
+
+    
 
 endmodule
